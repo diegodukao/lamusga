dfe205207c36b3864cd5e916425fb74f2f46b030
Diego Dukão <diegodukao@gmail.com>
2016-12-29 18:57:25 -0200

    playing mp3

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from kivy.app import App
+from kivy.core.audio import SoundLoader
+from kivy.uix.screenmanager import Screen
 from kivymd.theming import ThemeManager
+
+
+class Player(Screen):
+    def play(self):
+        sound = SoundLoader.load('music.mp3')
+        sound.play()
 
 
 class Lamusga(App):
