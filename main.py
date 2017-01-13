7964aedf524a0171e9333e7efad24943ea175c09
Diego Dukão <diegodukao@gmail.com>
2017-01-13 19:49:17 -0200

    stop button working

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from kivy.app import App
 from kivy.core.audio import SoundLoader
+from kivy.properties import ObjectProperty
 from kivy.uix.screenmanager import Screen
 from kivymd.theming import ThemeManager
 
 
 class Player(Screen):
+    playing = ObjectProperty()
+
     def play(self):
-        sound = SoundLoader.load('music.mp3')
-        sound.play()
+        self.playing = SoundLoader.load('music.mp3')
+        self.playing.play()
+
+    def stop(self):
+        self.playing.stop()
 
 
 class Lamusga(App):
