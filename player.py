f2c2f073691be26a5b2a6037074ae3562d27e9e2
Diego Dukão <diegodukao@gmail.com>
2017-02-16 14:11:57 -0200

    opening file and playing (y)

--- player.py
+++ player.py
@@ -1,9 +1,16 @@
 from kivy.core.audio import SoundLoader
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty, StringProperty
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.popup import Popup
 from kivy.uix.screenmanager import Screen
 
 Builder.load_file('ui/player.kv')
+
+
+class FileDialog(FloatLayout):
+    load = ObjectProperty()
+    cancel = ObjectProperty()
 
 
 class Player(Screen):
@@ -17,14 +24,31 @@ class Player(Screen):
             self.state = "stop"
 
         if self.state == "stop":
-            self.playing = SoundLoader.load('music.mp3')
-            self.playing.volume = 0
             self.playing.play()
             self.state = "play"
         else:
             self.playing.stop()
             self.state = "stop"
 
-    def stop(self):
-        self.playing.stop()
+    def play(self, filepath):
+        self.playing = SoundLoader.load(filepath)
+        self.playing.bind(on_stop=self.on_stop_callback)
+        self.playing.volume = 0.1
+        self.playing.play()
+        self.state = "play"
+
+    def dismiss_popup(self):
+        self._popup.dismiss()
+
+    def open_file_dialog(self):
+        content = FileDialog(load=self.load, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Load File", content=content,
+                            size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def load(self, path, filename):
+        self.play(filename[0])
+        self.dismiss_popup()
+
+    def on_stop_callback(self, obj):
         self.state = "stop"
